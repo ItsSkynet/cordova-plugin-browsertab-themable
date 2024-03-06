ca19f15470cc65466598e239756090b72452b5bd
Skynet <nzureuz@gmail.com>
2024-03-06 02:34:36 -0600

    Update BrowserTab.java

--- src/android/BrowserTab.java
+++ src/android/BrowserTab.java
@@ -99,6 +99,12 @@ public class BrowserTab extends CordovaPlugin {
       return;
     }
 
+    String customTabsBrowser = findCustomTabBrowser();
+    if (customTabsBrowser == null) {
+      Log.d(LOG_TAG, "openUrl: no in app browser tab available");
+      callbackContext.error("no in app browser tab implementation available");
+    }
+
     JSONObject themeableArgs;
     try {
         themeableArgs = new JSONObject(args.optString(1));
@@ -110,19 +116,25 @@ public class BrowserTab extends CordovaPlugin {
         Log.d(LOG_TAG, "openUrl themeableArgs: failed to parse theme parameters" + args);
     }
 
-    String customTabsBrowser = findCustomTabBrowser();
-    if (customTabsBrowser == null) {
-      Log.d(LOG_TAG, "openUrl: no in app browser tab available");
-      callbackContext.error("no in app browser tab implementation available");
-    }
+    // Initialize Builder
+    CustomTabsIntent.Builder customTabsIntentBuilder = new CustomTabsIntent.Builder();
 
-    CustomTabsIntent.Builder builder = new CustomTabsIntent.Builder();
-    builder.setToolbarColor( Color.parseColor(toolbarColor));
-    builder.addDefaultShareMenuItem();
-    Intent customTabsIntent = builder.build().intent;
+    // Set tab color
+    String tabColor = cordova.getActivity().getString(cordova.getActivity().getResources().getIdentifier("CUSTOM_TAB_COLOR_RGB", "string", cordova.getActivity().getPackageName()));
+    customTabsIntentBuilder.setToolbarColor(Color.parseColor(toolbarColor));
+
+    // Create Intent
+    Intent customTabsIntent = customTabsIntentBuilder.build().intent;
     customTabsIntent.setData(Uri.parse(urlStr));
-    customTabsIntent.setPackage(mCustomTabsBrowser);
-    cordova.getActivity().startActivity(customTabsIntent);
+    customTabsIntent.setPackage("com.android.chrome");
+    try {
+      cordova.getActivity().startActivity(customTabsIntent);
+    } catch (ActivityNotFoundException ex) {
+      customTabsIntent.setPackage(mCustomTabsBrowser);
+      cordova.getActivity().startActivity(customTabsIntent);
+    }
+    // Load URL
+    // customTabsIntent.launchUrl(cordova.getActivity(), Uri.parse(urlStr));
 
     Log.d(LOG_TAG, "in app browser call dispatched");
     callbackContext.success();
